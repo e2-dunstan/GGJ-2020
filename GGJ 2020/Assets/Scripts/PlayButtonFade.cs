12cbb98f2923fa495d04391c824cb6850a42c836
Tom <tom.harrison@live.com>
2020-01-31 22:53:41 +0000

    Fade to White Transition from menu

--- GGJ 2020/Assets/Scripts/PlayButtonFade.cs
+++ GGJ 2020/Assets/Scripts/PlayButtonFade.cs
@@ -6,12 +6,17 @@ using UnityEngine.SceneManagement;
 
 public class PlayButtonFade : MonoBehaviour
 {
+    [SerializeField] private SpriteRenderer ftw;
     private Text playText;
     private Color alphaIncrease = new Color(0, 0, 0, 0.002f);
+    private float floatAlphaIncrease = 1f;
+    private Color ftwAlphaIncrease;
+
 
     private void Awake()
     {
         playText = this.gameObject.GetComponent<Text>();
+        ftwAlphaIncrease = new Color(0, 0, 0, floatAlphaIncrease);
     }
 
     void Update()
@@ -22,12 +27,28 @@ public class PlayButtonFade : MonoBehaviour
         }
         if (Input.GetKeyDown(KeyCode.A))
         {
-            NextScene();
+            StartCoroutine(NextScene());
         }
     }
 
-    public void NextScene()
+    public void NextSceneButton()
     {
+        StartCoroutine(NextScene());
+    }
+
+    public IEnumerator NextScene()
+    {
+        Color startColour = new Color(ftw.color.r, ftw.color.g, ftw.color.b, 0);
+        Color endColour = new Color(ftw.color.r, ftw.color.g, ftw.color.b, 1);
+
+        for (float t = 0.0001f; t < 1.0; t += Time.deltaTime)
+        {
+            float progress = t / 1.0f;
+            ftw.color = startColour + (endColour - startColour) * progress;
+            yield return null;
+        }
+        ftw.color = endColour;
         SceneManager.LoadScene(1);
+        yield return null;
     }
 }
