029495b0e5d9e03b02411eaf907c81d6b3bfda93
Ellie <ellie2.dunstan@live.uwe.ac.uk>
2020-01-31 22:24:04 +0000

    Merge 2

---
+++ GGJ 2020/Assets/Scripts/Tool.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tool : MonoBehaviour
+{
+    public GameObject tool;
+    private Vector3 spawn;
+
+    private void Start()
+    {
+        spawn = tool.transform.position;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        GameObject newTool = Instantiate(tool, transform);
+        newTool.transform.position = spawn;
+    }
+}
