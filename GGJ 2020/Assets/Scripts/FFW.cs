994d773b174560c2fa4e6dd04fa92e66220d96b9
Tom <tom.harrison@live.com>
2020-01-31 23:12:57 +0000

    FFW for scene transition
    its a prefab

---
+++ GGJ 2020/Assets/Scripts/FFW.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FFW : MonoBehaviour
+{
+    private Color ffw;
+    [SerializeField] private SpriteRenderer sprite;
+
+    private void Awake()
+    {
+        StartCoroutine(Ffw());
+    }
+
+    private IEnumerator Ffw()
+    {
+        Color startColour = new Color(sprite.color.r, sprite.color.g, sprite.color.b, 1);
+        Color endColour = new Color(sprite.color.r, sprite.color.g, sprite.color.b, 0);
+
+        for (float t = 0.0001f; t < 1.0f; t += Time.deltaTime)
+        {
+            float progress = t / 1.0f;
+            sprite.color = startColour + (endColour - startColour) * progress;
+            yield return null;
+        }
+        sprite.color = endColour;
+        yield return null;
+    }
+}
