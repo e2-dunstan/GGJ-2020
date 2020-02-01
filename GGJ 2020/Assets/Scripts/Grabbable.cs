3251fe37b1b342c237d18c47ad3e0be902b676de
Elliott <elliottsmith1@hotmail.co.uk>
2020-02-01 11:19:02 +0000

    grabby grab

--- GGJ 2020/Assets/Scripts/Grabbable.cs
+++ GGJ 2020/Assets/Scripts/Grabbable.cs
@@ -5,16 +5,46 @@ using UnityEngine;
 
 public class Grabbable : MonoBehaviour
 {
+    [SerializeField] bool isTool = true;
     private LineRenderer lRenderer;
     private SpringJoint spring;
 
-    private bool attached = true;
+    public bool attached {private set; get;}
 
     // Start is called before the first frame update
     void Start()
     {
+        attached = false;
+
+        if (!isTool)
+        {
+            SetupUmbilical();
+        }
+    }
+
+    private void SetupUmbilical()
+    {
+        attached = true;
+
         lRenderer = GetComponent<LineRenderer>();
         lRenderer.SetPosition(0, transform.position);
+        Keyframe[] frames = new Keyframe[10];
+
+        float frame = 0.0f;
+        float min = 0.0f;
+        float max = 0.1f;
+
+        for (int i = 0; i < 10; i++)
+        {
+            float rand = UnityEngine.Random.Range(min, max);
+            Keyframe key = new Keyframe(frame, rand);
+            frame += 0.1f;
+            frames[i] = key;
+            min += 0.05f;
+            max += 0.05f;
+        }
+
+        lRenderer.widthCurve = new AnimationCurve(frames);
 
         spring = GetComponent<SpringJoint>();
     }
@@ -28,7 +58,10 @@ public class Grabbable : MonoBehaviour
 
     private void UpdateLineRenderer()
     {
-        lRenderer.SetPosition(1, transform.position);
+        for (int i = 1; i < lRenderer.positionCount; i++)
+        {
+            lRenderer.SetPosition(i, Vector3.Lerp(lRenderer.GetPosition(0), transform.position, 0.1f * i));
+        }
 
         if (!spring)
         {
