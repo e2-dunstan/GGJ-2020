3ba5fecb5692cbe071a6e891a688c920a09eab82
Tom <tom.harrison@live.com>
2020-02-01 13:43:32 +0000

    Blood expanding and model with hole in it

---
+++ GGJ 2020/Assets/Scripts/BloodColliison.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BloodColliison : MonoBehaviour
+{
+
+    [SerializeField] private GameObject bloodPrefab;
+    [SerializeField] private ParticleSystem particleSystem;
+    public List<ParticleCollisionEvent> collisionEvents;
+
+    void Start()
+    {
+        collisionEvents = new List<ParticleCollisionEvent>();
+    }
+
+    void Update()
+    {
+        
+    }
+
+    private void OnParticleCollision(GameObject other)
+    {
+        int numCollisionEvents = particleSystem.GetCollisionEvents(other, collisionEvents);
+        if (other.tag != "Blood" && other.tag != "BloodParticle")
+        {
+            Instantiate(bloodPrefab, collisionEvents[0].intersection, Quaternion.identity);
+        }
+        else
+        {
+            other.gameObject.GetComponent<BloodSetUp>().IncreaseBlood();
+        }
+    }
+
+}
---
+++ GGJ 2020/Assets/Scripts/BloodSetUp.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BloodSetUp : MonoBehaviour
+{
+    [SerializeField]private SpriteRenderer sprite;
+    private float size = 0.1f;
+    private float blobsHit = 0;
+    private Vector3 startSize = new Vector3(0.1f, 0.1f, 1);
+
+    private void Awake()
+    {
+        StartCoroutine(LivingFade());
+    }
+
+    public void IncreaseBlood()
+    {
+        blobsHit++;
+
+        if (blobsHit >= 11)
+        {
+            return;
+        }
+        else
+        {
+            size = 0.1f * blobsHit;
+            Vector3 scale = new Vector3(size, size, 1);
+            this.gameObject.transform.localScale = scale;
+        }
+    }
+
+    private IEnumerator LivingFade()
+    {
+        yield return new WaitForSeconds(3.0f);
+
+        Color startColour = new Color(sprite.color.r, sprite.color.g, sprite.color.b, 1);
+        Color endColour = new Color(sprite.color.r, sprite.color.g, sprite.color.b, 0);
+
+        for (float t = 0.0001f; t < 1.0; t += Time.deltaTime)
+        {
+            float progress = t / 1.0f;
+            sprite.color = startColour + (startColour - endColour) * progress;
+            yield return null;
+        }
+        sprite.color = endColour;
+        Destroy(this.gameObject.transform.parent.gameObject);
+        yield return null;
+    }
+}
