d457d2a3261ca73575a85be60c700fc1462c6af3
Jack <jack2.strowger@live.uwe.ac.uk>
2020-02-01 11:50:03 +0000

    Unpacked Hand + Test Force for ragdoll

---
+++ GGJ 2020/Assets/TestRagdoll.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestRagdoll : MonoBehaviour
+{
+    public GameObject LeftLeg;
+    public GameObject RightLeg;
+
+    void Start()
+    {
+        LeftLeg = GameObject.Find("mixamorig1:LeftLeg");
+        RightLeg = GameObject.Find("mixamorig1:RightLeg");
+    }
+   
+         
+   
+
+    // Update is called once per frame
+    void Update()
+    {
+       if (Input.GetAxisRaw("Fire1") != 0)
+        {
+            LeftLeg.GetComponent<Rigidbody>().AddForce(new Vector3(0, 0, -25));
+            RightLeg.GetComponent<Rigidbody>().AddForce(new Vector3(0, 0, -25));
+        }
+
+    }
+}
