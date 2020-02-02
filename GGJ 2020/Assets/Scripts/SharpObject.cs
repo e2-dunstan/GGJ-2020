6e7ab370536ac5a96dc427603e5a2f49b0a77c32
Elliott <elliottsmith1@hotmail.co.uk>
2020-02-02 11:15:34 +0000

    cutting ( ͡° ͜ʖ ͡°)

---
+++ GGJ 2020/Assets/Scripts/SharpObject.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SharpObject : MonoBehaviour
+{
+    [SerializeField] GameObject bloodPrefab;
+    [SerializeField] Transform bladePoint;
+
+    private float bloodSpawnDelay = 1.0f;
+    private float bloodSpawnTimer = 0.0f;
+    private bool spawned = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (spawned)
+        {
+            bloodSpawnTimer += Time.deltaTime;
+
+            if (bloodSpawnTimer > bloodSpawnDelay)
+            {
+                bloodSpawnTimer = 0.0f;
+                spawned = false;
+            }
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!spawned)
+        {
+            if (other.tag != "Surface")
+            {
+                spawned = true;
+                GameObject blood = Instantiate(bloodPrefab, bladePoint.position, Quaternion.identity);
+            }
+        }
+    }
+}
